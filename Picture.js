b88f45259df78d3fa58c5f643714377202feed93
Marnay1 <87884789+Marnay1@users.noreply.github.com>
2022-06-19 17:32:33 -0400

    Add files via upload

--- Picture.js
+++ Picture.js
@@ -1,15 +1,28 @@
 status= "";
+objects= [];
 function preload(){
     img= loadImage("Pictures.jpg");
 }
 function setup(){
-    canvas= createCanvas(640, 340);
+    canvas= createCanvas(640, 400);
     canvas.center();
     objectDetector= ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML= "Status : Detecting Objects";
 }
 function draw(){
-    image(img, 0, 0, 640, 340);
+    image(img, 0, 0, 640, 400);
+    if(status != ""){
+        for (i=0; i< objects.length; i++){
+            document.getElementById("objects_there").innerHTML= "There are six identified objects in this picture and cocossd identified " + objects.length +"objects"; 
+            confidence= floor(objects[i].confidence * 100);
+            label= objects[i].label;
+            fill("blue");
+            text(label+ " "+ confidence + "%", objects[i].x +20, objects[i].y + 20);
+            noFill();
+            stroke("blue");
+            rect(objects[i].width, objects[i].height, objects[i].x, objects[i].y);
+        }
+    }
 }
 function modelLoaded(){
     console.log("Model Loaded");
@@ -21,4 +34,5 @@ function gotResult(error, results){
         console.log(error);
     }
     console.log(results);
+    objects= results;
 }
